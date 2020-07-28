773d5481ab3ac472cd8c1e9b740d2a1de6d5a5a3
WilliamsMayorga <scrumsoft3g@gmail.com>
2020-07-28 17:02:36 -0400

    Implementando CRUD en el controlador métodos handler para post, put y delete

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -3,9 +3,14 @@ package com.formacionbdi.microservicios.app.usuarios.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
@@ -26,8 +31,34 @@ public class AlumnoController {
 	public ResponseEntity<?> ver(@PathVariable Long id){
 		Optional<Alumno> optionalAlumno = service.findById(id);
 		if(!optionalAlumno.isPresent()) {
-			ResponseEntity.notFound().build();
+			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok(optionalAlumno.get());
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> crear(@RequestBody Alumno alumno){
+		Alumno alumnoDb = service.save(alumno);
+		return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDb);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id){
+		Optional<Alumno> optionalAlumno = service.findById(id);
+		if(!optionalAlumno.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Alumno alumnoDb = optionalAlumno.get();
+		alumnoDb.setNombre(alumno.getNombre());
+		alumnoDb.setApellido(alumno.getApellido());
+		alumnoDb.setEmail(alumno.getEmail());
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> eliminar(@PathVariable Long id){
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
