b376408bbed9d4959c7bd918299a04288658daa6
WilliamsMayorga <scrumsoft3g@gmail.com>
2020-07-28 00:06:40 -0400

    Creando el repositorio JPA para los alumnos

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/models/entity/Alumno.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/models/entity/Alumno.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="alumnos")
@@ -23,6 +25,7 @@ public class Alumno {
 	private String email;
 	
 	@Column(name = "create_at")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 
 	public void prePersist() {
